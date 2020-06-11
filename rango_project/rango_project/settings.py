eebdd31bf29b61efa3a1430b80ed8a6f0d363e6c
Prabhat <prabhat.katiyar@fractal.ai>
2020-06-11 15:13:40 +0530

    chapter 10 Bootstraping

--- rango_project/rango_project/settings.py
+++ rango_project/rango_project/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rango',
-    'registration'
+    'registration',
+    'bootstrap_toolkit',
 ]
 
 MIDDLEWARE_CLASSES = [
